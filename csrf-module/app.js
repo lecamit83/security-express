929ff2bad9f046d20397a68be5e0f66494b14d11
Le Cam <thantoandoc83@gmail.com>
2019-07-09 16:56:03 +0700

    add view engine ejs

--- csrf-module/app.js
+++ csrf-module/app.js
@@ -1,25 +1,44 @@
 const express = require('express');
 const session = require('express-session');
 const csrf = require('csurf');
-
+const bodyParser = require('body-parser');
 const app = express();
+
 app.use(session({
   resave: false,
   secret : 'sdadsadas',
-  saveUninitialized : true
-}))
-app.use(csrf({cookie : true}));
+  saveUninitialized : true,
+  cookie : {
+    maxAge: 30000
+  }
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(csrf());
 
-// create csrf token when someone request to any routing
-app.use(function(req, res, next){
-  res.cookie('CSRF-TOKEN', req.csrfToken());
-  next();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', function(req, res){
+  res.render('form', {
+    csrfToken : req.csrfToken()
+  });
+});
+
+app.post('/submit', function(req, res){
+  res.send('/submit');
 });
 
 app.use(function(err, req, res, next){
-  res.status(500);
-  res.send('Internal Interrup!')
+  if(err.code !== "EBADCSRFTOKEN"){
+    next(err);
+    return;
+  }
+  res.status(403).send('CSRF error');
 });
+app.use(function(err, req, res, next){
+  res.status(500).send('Internal Error!'); 
+})
 app.listen(3000, function(){
   console.log('Server is running...');
 });
